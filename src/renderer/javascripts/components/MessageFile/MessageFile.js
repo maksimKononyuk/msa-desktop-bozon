9f55f6451a6ea2679c0243e504bcbe0146a7e2db
Maxim Kononyuk <m.kononyuk@msaplatform.si>
2022-12-20 14:41:24 +0300

    added modal to image in reports

--- src/renderer/javascripts/components/MessageFile/MessageFile.js
+++ src/renderer/javascripts/components/MessageFile/MessageFile.js
@@ -1,34 +1,85 @@
-import React, { useState } from 'react'
-import { View, Image, ActivityIndicator } from 'react-native-web'
+import React, { useState, useMemo } from 'react'
+import {
+  View,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+  Modal
+} from 'react-native-web'
 import iconFile from '../../assets/icons/file.png'
 import styles from './styles'
 
 const MessageFile = ({ uri }) => {
   const [isIconLoading, setIsIconLoading] = useState(true)
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const isImage = useMemo(() => {
+    if (
+      uri.toLowerCase().includes('jpg') ||
+      uri.toLowerCase().includes('jpeg') ||
+      uri.toLowerCase().includes('png') ||
+      uri.toLowerCase().includes('gif')
+    )
+      return true
+    else return false
+  }, [uri])
 
   return (
-    <View style={styles.pickerBlock}>
-      <Image
-        onLoadEnd={() => setIsIconLoading(false)}
-        source={
-          uri.toLowerCase().includes('jpg') ||
-          uri.toLowerCase().includes('jpeg') ||
-          uri.toLowerCase().includes('png') ||
-          uri.toLowerCase().includes('gif')
-            ? {
-                uri: uri
-              }
-            : iconFile
-        }
-        style={styles.pickerContainer}
-        resizeMode='cover'
-      />
-      {isIconLoading && (
-        <View style={styles.activityIndicator}>
-          <ActivityIndicator size='large' color='#000088' />
-        </View>
-      )}
-    </View>
+    <>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        style={styles.pickerBlock}
+        onPress={() => {
+          isImage && setIsModalVisible(true)
+        }}
+      >
+        <Image
+          onLoadEnd={() => setIsIconLoading(false)}
+          source={
+            uri.toLowerCase().includes('jpg') ||
+            uri.toLowerCase().includes('jpeg') ||
+            uri.toLowerCase().includes('png') ||
+            uri.toLowerCase().includes('gif')
+              ? {
+                  uri: uri
+                }
+              : iconFile
+          }
+          style={styles.pickerContainer}
+          resizeMode='cover'
+        />
+        {isIconLoading && (
+          <View style={styles.activityIndicator}>
+            <ActivityIndicator size='large' color='#000088' />
+          </View>
+        )}
+      </TouchableOpacity>
+      <Modal visible={isModalVisible} animationType='slide' transparent={false}>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => setIsModalVisible(false)}
+          style={styles.modalCancelBlock}
+        >
+          <View
+            style={[
+              styles.closeModalButtonLeftLine,
+              styles.closeModalButtonLine
+            ]}
+          />
+          <View
+            style={[
+              styles.closeModalButtonRightLine,
+              styles.closeModalButtonLine
+            ]}
+          />
+        </TouchableOpacity>
+        <Image
+          source={{ uri }}
+          style={{ width: '95%', height: '95%' }}
+          resizeMode='contain'
+        />
+      </Modal>
+    </>
   )
 }
 
--- src/renderer/javascripts/components/MessageFile/styles.js
+++ src/renderer/javascripts/components/MessageFile/styles.js
@@ -16,6 +16,29 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     justifyContent: 'center'
+  },
+  modalCancelBlock: {
+    position: 'absolute',
+    top: '2%',
+    right: '3%',
+    zIndex: 20,
+    width: 40,
+    height: 40,
+    backgroundColor: '#000',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  closeModalButtonLine: {
+    width: 25,
+    height: 2,
+    backgroundColor: '#fff'
+  },
+  closeModalButtonLeftLine: {
+    transform: [{ rotate: '45deg' }, { translateY: 1 }]
+  },
+  closeModalButtonRightLine: {
+    transform: [{ rotate: '-45deg' }, { translateY: -1 }]
   }
 })
 
